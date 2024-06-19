98e5cb151e58978f5e4ea188947046f9b0b12eae
nandobfer <nandobfer@yahoo.com.br>
2024-06-19 07:57:03 -0300

    update: componetized resale accordion

--- src/Screens/Resale/ManagersContainer.tsx
+++ src/Screens/Resale/ManagersContainer.tsx
@@ -1,9 +1,10 @@
 import React, { useState } from "react"
-import { LayoutAnimation, View } from "react-native"
+import { View } from "react-native"
 import { User } from "../../types/server/class"
-import { IconButton, List, Text, useTheme } from "react-native-paper"
+import { Text } from "react-native-paper"
 import { ManagerFormModal } from "./ManagerFormModal"
 import { Resale } from "../../types/server/class/Resale"
+import { ResaleAccordion } from "./ResaleAccordion"
 
 interface ManagersContainerProps {
     managers: User[]
@@ -12,40 +13,23 @@ interface ManagersContainerProps {
 }
 
 export const ManagersContainer: React.FC<ManagersContainerProps> = ({ managers, resale, onAddManager }) => {
-    const theme = useTheme()
-    const [expanded, setExpanded] = useState(true)
     const [showManagerForm, setShowManagerForm] = useState(false)
-
-    const onPress = () => {
-        LayoutAnimation.configureNext(LayoutAnimation.Presets.easeInEaseOut)
-        setExpanded((value) => !value)
-    }
 
     return (
         <View style={[{ marginHorizontal: -20 }]}>
-            <List.Accordion
-                style={[{ backgroundColor: theme.colors.elevation.level1 }]}
-                expanded={expanded}
-                title={
-                    <View style={[{ flexDirection: "row", alignItems: "center" }]}>
-                        <IconButton icon={expanded ? "chevron-up" : "chevron-down"} style={{ margin: 0 }} />
-                        <Text variant="labelLarge">Administradores da revenda</Text>
-                    </View>
+            <ResaleAccordion
+                title="Administradores da revenda"
+                onAddIconPress={() => setShowManagerForm(true)}
+                modal={
+                    <ManagerFormModal visible={showManagerForm} onDismiss={() => setShowManagerForm(false)} resale={resale} onFinish={onAddManager} />
                 }
-                onPress={onPress}
-                pointerEvents="box-none"
-                right={(expanded) => <IconButton icon="plus" style={{ margin: 0 }} onPress={() => setShowManagerForm(true)} />}
             >
-                <View style={[{ paddingHorizontal: 30 }]}>
-                    {managers.map((item) => (
-                        <Text variant="labelLarge" key={item.id}>
-                            {item.name}
-                        </Text>
-                    ))}
-                </View>
-            </List.Accordion>
-
-            <ManagerFormModal visible={showManagerForm} onDismiss={() => setShowManagerForm(false)} resale={resale} onFinish={onAddManager} />
+                {managers.map((item) => (
+                    <Text variant="labelLarge" key={item.id}>
+                        {item.name}
+                    </Text>
+                ))}
+            </ResaleAccordion>
         </View>
     )
 }
---
+++ src/Screens/Resale/ResaleAccordion.tsx
@@ -0,0 +1,41 @@
+import React, { useState } from "react"
+import { LayoutAnimation, View } from "react-native"
+import { IconButton, List, Text, useTheme } from "react-native-paper"
+
+interface ResaleAccordionProps {
+    title: string
+    onAddIconPress: () => void
+    children?: React.ReactNode
+    modal?: React.ReactNode
+}
+
+export const ResaleAccordion: React.FC<ResaleAccordionProps> = ({ title, onAddIconPress, children, modal }) => {
+    const theme = useTheme()
+
+    const [expanded, setExpanded] = useState(true)
+
+    const onPress = () => {
+        LayoutAnimation.configureNext(LayoutAnimation.Presets.easeInEaseOut)
+        setExpanded((value) => !value)
+    }
+
+    return (
+        <List.Accordion
+            style={[{ backgroundColor: theme.colors.elevation.level1 }]}
+            expanded={expanded}
+            title={
+                <View style={[{ flexDirection: "row", alignItems: "center" }]}>
+                    <IconButton icon={expanded ? "chevron-up" : "chevron-down"} style={{ margin: 0 }} />
+                    <Text variant="labelLarge">{title}</Text>
+                </View>
+            }
+            onPress={onPress}
+            pointerEvents="box-none"
+            right={(expanded) => <IconButton icon="plus" style={{ margin: 0 }} onPress={onAddIconPress} />}
+        >
+            <View style={[{ paddingHorizontal: 30 }]}>{children}</View>
+
+            {modal}
+        </List.Accordion>
+    )
+}
