a4c55ec909b073cfa0a63126f3e247e1de45f03c
alfredo-petri <alfredo_petri@hotmail.com>
2024-06-19 16:00:18 -0300

    update ResaleListModal

--- src/Screens/Admin/ResaleListModal.tsx
+++ src/Screens/Admin/ResaleListModal.tsx
@@ -1,6 +1,10 @@
-import React from "react"
-import { Platform } from "react-native"
-import { Modal, Portal, Surface, useTheme } from "react-native-paper"
+import React, { useEffect, useState } from "react"
+import { Platform, View } from "react-native"
+import { Button, Icon, IconButton, Modal, Portal, Surface, Text, TextInput, useTheme } from "react-native-paper"
+import { colors } from "../../style/colors"
+import { useResale } from "../../hooks/useResale"
+import { Resale } from "../../types/server/class/Resale"
+import { ResaleCard } from "./ResaleCard"
 
 interface ResaleListModalProps {
     visible: boolean
@@ -9,15 +13,38 @@ interface ResaleListModalProps {
 
 export const ResaleListModal: React.FC<ResaleListModalProps> = ({ visible, onDismiss }) => {
     const theme = useTheme()
+    const { fetchAllResales } = useResale()
+
+    const [loading, setLoading] = useState(false)
+    const [resales, setResales] = useState<Resale[]>([])
+
+    const fetchResales = async () => {
+        setLoading(true)
+        try {
+            const data = await fetchAllResales()
+            setResales(data)
+            // setSelectedResale(data.sort((a, b) => Number(b.created_at) - Number(a.created_at))[0])
+        } catch (error) {
+            console.log(error)
+        } finally {
+            setLoading(false)
+        }
+    }
+
+    useEffect(() => {
+        fetchResales()
+    }, [])
+
     return (
         <Portal>
             <Modal
                 visible={visible}
                 onDismiss={onDismiss}
                 style={{
-                    height: 200,
+                    marginTop: 200,
                     marginLeft: 145,
-                    width: 400,
+                    height: 70,
+                    width: 470,
                 }}
                 contentContainerStyle={[
                     {
@@ -27,7 +54,25 @@ export const ResaleListModal: React.FC<ResaleListModalProps> = ({ visible, onDis
                     // Platform.OS == "web" ? {} : { marginBottom: keyboardVisible ? 0 : undefined },
                 ]}
             >
-                <Surface style={{ padding: 20, gap: 20, borderRadius: 15 }}>Teste</Surface>
+                <View style={{ padding: 20, gap: 20, borderRadius: 15 }}>
+                    <View style={{ flexDirection: "row", gap: 10 }}>
+                        <TextInput
+                            mode="outlined"
+                            dense
+                            placeholder="Procurar por nome, CPF, CNPJ, email..."
+                            // value={searchValue}
+                            // onChangeText={(text) => setSearchValue(text)}
+                            right={<TextInput.Icon icon="magnify" />}
+                            style={[{ flex: 1 }]}
+                        />
+                        <View style={[{ borderColor: colors.grey, borderWidth: 1, borderRadius: 5 }]}>
+                            <IconButton icon="swap-vertical" iconColor={theme.colors.primary} />
+                        </View>
+                    </View>
+                    {resales.map((resale) => (
+                        <ResaleCard resale={resale} />
+                    ))}
+                </View>
             </Modal>
         </Portal>
     )
