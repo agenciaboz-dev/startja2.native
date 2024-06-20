08eaedeb6ec489fde3a1d3ee84610fe09f6a226d
alfredo-petri <alfredo_petri@hotmail.com>
2024-06-20 13:32:14 -0300

    create Expandbutton

---
+++ src/components/Tools/ExpandButton.tsx
@@ -0,0 +1,8 @@
+import React from "react"
+import { Surface } from "react-native-paper"
+
+interface ExpandButtonProps {}
+
+export const ExpandButton: React.FC<ExpandButtonProps> = ({}) => {
+    return <Surface style={{ flex: 1 }}>teste</Surface>
+}
