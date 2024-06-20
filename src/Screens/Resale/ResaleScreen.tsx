90fb5002ac559d9c6ec1c80cc64ffbf26505318f
alfredo-petri <alfredo_petri@hotmail.com>
2024-06-20 09:34:46 -0300

    create SettingsCard

--- src/Screens/Resale/ResaleScreen.tsx
+++ src/Screens/Resale/ResaleScreen.tsx
@@ -9,6 +9,7 @@ import { colors } from "../../style/colors"
 import { ManagersContainer } from "./ManagersContainer"
 import { CustomersAccordion } from "./CustomersAccordion"
 import { ResaleListModal } from "../Admin/ResaleListModal"
+import { SettingsCard } from "../Settings/SettingsCard"
 
 interface ResaleScreenProps {
     route: RouteProp<any, any>
@@ -65,6 +66,7 @@ export const ResaleScreen: React.FC<ResaleScreenProps> = ({ route }) => {
             </View>
             {resale && <ManagersContainer managers={managers} resale={resale} onAddManager={onAddManager} />}
             {resale && <CustomersAccordion resale={resale} />}
+            {/* <SettingsCard /> */}
             <ResaleListModal visible={resaleListModalOpen} onDismiss={() => setResaleListModalOpen(false)} />
         </Surface>
     )
