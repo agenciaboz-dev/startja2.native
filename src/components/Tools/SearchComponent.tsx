cb7ac692455eed225cf54ab9fc88e010e39962ff
alfredo-petri <alfredo_petri@hotmail.com>
2024-06-20 13:32:31 -0300

    create SearchComponent

---
+++ src/components/Tools/SearchComponent.tsx
@@ -0,0 +1,20 @@
+import React from "react"
+import { Surface, TextInput } from "react-native-paper"
+
+interface SearchComponentProps {
+    placeholder?: string
+}
+
+export const SearchComponent: React.FC<SearchComponentProps> = ({ placeholder = "Procurar por nome, CPF, CNPJ, email..." }) => {
+    return (
+        <TextInput
+            mode="outlined"
+            dense
+            placeholder={placeholder}
+            // value={searchValue}
+            // onChangeText={(text) => setSearchValue(text)}
+            right={<TextInput.Icon icon="magnify" />}
+            style={[{ flex: 1 }]}
+        />
+    )
+}
