e002c959eb2740a8cdf6abda6c194b0ecea68a7a
alfredo-petri <alfredo_petri@hotmail.com>
2024-06-20 13:45:23 -0300

    create placeholders

---
+++ src/components/Tools/placeholders.ts
@@ -0,0 +1,12 @@
+const user = require("../../../assets/placeholders/placeholder-user.png")
+const guest = require("../../../assets/placeholders/placeholder-guest")
+const admin = require("../../../assets/placeholders/placeholder-admin")
+const resale = require("../../../assets/placeholders/placeholder-resale")
+// const guest = require("../../../assets/placeholders/placeholder-guest")
+
+export default {
+    user,
+    guest,
+    admin,
+    resale,
+}
