4e36c8f5521a594ced47b3afa612480ae6c5a5d0
alfredo-petri <alfredo_petri@hotmail.com>
2024-06-20 15:29:09 -0300

    update placeholders

--- src/components/Tools/placeholders.ts
+++ src/components/Tools/placeholders.ts
@@ -1,7 +1,7 @@
 const user = require("../../../assets/placeholders/placeholder-user.png")
-const guest = require("../../../assets/placeholders/placeholder-guest")
-const admin = require("../../../assets/placeholders/placeholder-admin")
-const resale = require("../../../assets/placeholders/placeholder-resale")
+const guest = require("../../../assets/placeholders/placeholder-guest.png")
+const admin = require("../../../assets/placeholders/placeholder-admin.png")
+const resale = require("../../../assets/placeholders/placeholder-resale.png")
 // const guest = require("../../../assets/placeholders/placeholder-guest")
 
 export default {
