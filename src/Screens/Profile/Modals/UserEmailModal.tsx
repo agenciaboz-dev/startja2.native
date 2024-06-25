6bd9fb5923f80f57c44dc83db239f25baa27a507
alfredo-petri <alfredo_petri@hotmail.com>
2024-06-25 14:09:43 -0300

    update UserEmailModal

--- src/Screens/Profile/Modals/UserEmailModal.tsx
+++ src/Screens/Profile/Modals/UserEmailModal.tsx
@@ -1,6 +1,7 @@
-import React from "react"
-import { View } from "react-native"
-import { Modal, Portal } from "react-native-paper"
+import React, { useState } from "react"
+import { Platform, View } from "react-native"
+import { Button, IconButton, Modal, Portal, Text, TextInput } from "react-native-paper"
+import { colors } from "../../../style/colors"
 
 interface UserEmailModalProps {
     visible: boolean
@@ -8,10 +9,60 @@ interface UserEmailModalProps {
 }
 
 export const UserEmailModal: React.FC<UserEmailModalProps> = ({ visible, onDismiss }) => {
+    const [emailActive, setEmailActive] = useState(false)
+    const [newEmailActive, setNewEmailActive] = useState(false)
+    const [passwordActive, setPasswordActive] = useState(false)
+
     return (
         <Portal>
-            <Modal visible={visible} onDismiss={onDismiss}>
-                <View></View>
+            <Modal
+                visible={visible}
+                onDismiss={onDismiss}
+                contentContainerStyle={[
+                    {
+                        backgroundColor: colors.box,
+                        alignSelf: "center",
+                        padding: 20,
+                        gap: 20,
+                        borderRadius: 10,
+                    },
+                    Platform.OS == "web" && {},
+                ]}
+            >
+                <View style={{ gap: 20 }}>
+                    <View style={{ flexDirection: "row", alignItems: "center", justifyContent: "space-between" }}>
+                        <Text variant="headlineSmall">Alterar email do usuário</Text>
+                        <IconButton icon="close" onPress={onDismiss} />
+                    </View>
+                    <Text>Forneça um endereço de email válido para receber notificações e recuperar sua conta</Text>
+                    <TextInput
+                        mode="outlined"
+                        placeholder="Digite o endereço de email atual"
+                        label={emailActive ? "Endereço de email atual" : "Digite o endereço de email atual"}
+                        // label="Novo nome de usuário"
+                        onFocus={() => setEmailActive(true)}
+                        onBlur={() => setEmailActive(false)}
+                    />
+                    <TextInput
+                        mode="outlined"
+                        placeholder="Digite o novo endereço de email"
+                        label={newEmailActive ? "Novo email" : "Digite o novo endereço de email"}
+                        // label="Novo nome de usuário"
+                        onFocus={() => setNewEmailActive(true)}
+                        onBlur={() => setNewEmailActive(false)}
+                    />
+                    <TextInput
+                        mode="outlined"
+                        placeholder="Insira sua senha"
+                        label={passwordActive ? "Senha" : "Insira sua senha"}
+                        // label="Novo nome de usuário"
+                        onFocus={() => setPasswordActive(true)}
+                        onBlur={() => setPasswordActive(false)}
+                    />
+                    <Button mode="contained" style={{ alignSelf: "flex-end" }}>
+                        Atualizar
+                    </Button>
+                </View>
             </Modal>
         </Portal>
     )
