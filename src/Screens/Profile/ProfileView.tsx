a5c4322e4b6cad3299d350aaa3393df58f26cd53
alfredo-petri <alfredo_petri@hotmail.com>
2024-06-25 10:20:42 -0300

    Update ProfileView

--- src/Screens/Profile/ProfileView.tsx
+++ src/Screens/Profile/ProfileView.tsx
@@ -8,13 +8,24 @@ import { Image } from "expo-image"
 import { ProfileInfoCard } from "./ProfileInfoCard"
 import { colors } from "../../style/colors"
 import { PasswordModal } from "./Modals/PasswordModal"
+import { DisableAccountModal } from "./Modals/DisableAccountModal"
+import { DeleteAccountModal } from "./Modals/DeleteAccountModal"
+import { UserNameModal } from "./Modals/UserNameModal"
+import { UserEmailModal } from "./Modals/UserEmailModal"
+import { UserPhoneModal } from "./Modals/UserPhoneModal"
 
 interface ProfileViewProps {}
 
 export const ProfileView: React.FC<ProfileViewProps> = ({}) => {
     const { user } = useUser()
     const [hover, setHover] = useState(false)
+    const [openUserNameModal, setOpenUserNameModal] = useState(false)
+    const [openUserEmailModal, setOpenUserEmailModal] = useState(false)
+    const [openPhoneModal, setOpenPhoneModal] = useState(false)
     const [openPasswordModal, setOpenPasswordModal] = useState(false)
+    const [openDisableAccountModal, setOpenDisableAccountModal] = useState(false)
+    const [openDeleteAccountModal, setOpenDeleteAccountModal] = useState(false)
+
     return (
         <View style={{ flex: 1, paddingTop: 80, paddingLeft: 50, paddingRight: 500, gap: 40 }}>
             <View style={{ flexDirection: "row", justifyContent: "space-between", alignItems: "center" }}>
@@ -32,9 +43,9 @@ export const ProfileView: React.FC<ProfileViewProps> = ({}) => {
                     <Text variant="headlineSmall">{user?.name}</Text>
                 </View>
                 <View style={{ padding: 20, gap: 20, backgroundColor: colors.box }}>
-                    <ProfileInfoCard description="NOME DE USUÁRIO" data={user!.name} />
-                    <ProfileInfoCard description="EMAIL" data={user!.email} />
-                    <ProfileInfoCard description="TELEFONE" data={user!.phone} />
+                    <ProfileInfoCard description="NOME DE USUÁRIO" data={user!.name} onPress={() => setOpenUserNameModal(true)} />
+                    <ProfileInfoCard description="EMAIL" data={user!.email} onPress={() => setOpenUserEmailModal(true)} />
+                    <ProfileInfoCard description="TELEFONE" data={user!.phone} onPress={() => setOpenPhoneModal(true)} />
                 </View>
             </Surface>
             <View style={{ gap: 15 }}>
@@ -47,15 +58,20 @@ export const ProfileView: React.FC<ProfileViewProps> = ({}) => {
                 <Text variant="headlineSmall">Deletar conta</Text>
                 <Text> Desativar sua conta significa que você pode recuperá-la a qualquer momento após realizar esta ação</Text>
                 <View style={{ flexDirection: "row", gap: 15 }}>
-                    <Button mode="contained-tonal" textColor={colors.dark}>
+                    <Button mode="contained-tonal" textColor={colors.dark} onPress={() => setOpenDisableAccountModal(true)}>
                         Desabilitar Conta
                     </Button>
-                    <Button mode="contained" buttonColor={colors.grey} dark>
+                    <Button mode="contained" buttonColor={colors.grey} dark onPress={() => setOpenDeleteAccountModal(true)}>
                         Deletar Conta
                     </Button>
                 </View>
             </View>
+            <UserNameModal visible={openUserNameModal} onDismiss={() => setOpenUserNameModal(false)} />
+            <UserEmailModal visible={openUserEmailModal} onDismiss={() => setOpenUserEmailModal(false)} />
+            <UserPhoneModal visible={openPhoneModal} onDismiss={() => setOpenPhoneModal(false)} />
             <PasswordModal visible={openPasswordModal} onDismiss={() => setOpenPasswordModal(false)} />
+            <DisableAccountModal visible={openDisableAccountModal} onDismiss={() => setOpenDisableAccountModal(false)} />
+            <DeleteAccountModal visible={openDeleteAccountModal} onDismiss={() => setOpenDeleteAccountModal(false)} />
         </View>
     )
 }
