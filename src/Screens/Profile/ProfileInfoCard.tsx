af664bace42aca6267041f7bc32415661afa7b92
alfredo-petri <alfredo_petri@hotmail.com>
2024-06-25 10:20:20 -0300

    update ProfileInfoCard

--- src/Screens/Profile/ProfileInfoCard.tsx
+++ src/Screens/Profile/ProfileInfoCard.tsx
@@ -5,16 +5,19 @@ import { Button, Text } from "react-native-paper"
 interface ProfileInfoCardProps {
     description: string
     data: string
+    onPress: () => void
 }
 
-export const ProfileInfoCard: React.FC<ProfileInfoCardProps> = ({ description, data }) => {
+export const ProfileInfoCard: React.FC<ProfileInfoCardProps> = ({ description, data, onPress }) => {
     return (
         <View style={{ flexDirection: "row", justifyContent: "space-between", alignItems: "center" }}>
             <View>
                 <Text>{description}</Text>
                 <Text>{data}</Text>
             </View>
-            <Button mode="contained">Editar</Button>
+            <Button mode="contained" onPress={onPress}>
+                Editar
+            </Button>
         </View>
     )
 }
