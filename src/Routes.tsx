1664d5548da1ca1470d09bc7c38a73c3c3d3af17
ispadilha <israel.padilha@gmail.com>
2024-06-26 11:08:08 -0300

    feat: BottomNav components

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react"
+import React from "react"
 import { createNativeStackNavigator } from "@react-navigation/native-stack"
 import { default_navigator_options } from "./tools/default_navigator_options"
 import { Platform } from "react-native"
@@ -7,6 +7,8 @@ import { NotFound } from "./Screens/NotFound"
 import { SystemChooser } from "./Screens/SystemChooser/SystemChooser"
 import { AdminScreen } from "./Screens/Admin/AdminScreen"
 import { Profile } from "./Screens/Profile/Profile"
+import { useUser } from "./hooks/useUser"
+import { BottomNav } from "./components/BottomNav/BottomNav"
 
 interface RoutesProps {}
 
@@ -21,21 +23,25 @@ export type HomeStackParams = {
 const Stack = createNativeStackNavigator<HomeStackParams>()
 
 export const Routes: React.FC<RoutesProps> = ({}) => {
-    const ios = Platform.OS == "ios"
+    const { user } = useUser()
 
     return (
-        <Stack.Navigator
-            screenOptions={{
-                ...default_navigator_options,
-                header: undefined,
-                headerShown: false,
-            }}
-        >
-            <Stack.Screen name="home" component={Home} />
-            <Stack.Screen name="notfound" component={NotFound} />
-            <Stack.Screen name="systemChooser" component={SystemChooser} />
-            <Stack.Screen name="admin" component={AdminScreen} />
-            <Stack.Screen name="profile" component={Profile} />
-        </Stack.Navigator>
+        <>
+            <Stack.Navigator
+                screenOptions={{
+                    ...default_navigator_options,
+                    header: undefined,
+                    headerShown: false,
+                    gestureEnabled: false,
+                }}
+            >
+                <Stack.Screen name="home" component={Home} />
+                <Stack.Screen name="notfound" component={NotFound} />
+                <Stack.Screen name="systemChooser" component={SystemChooser} />
+                <Stack.Screen name="admin" component={AdminScreen} />
+                <Stack.Screen name="profile" component={Profile} />
+            </Stack.Navigator>
+            {user && Platform.OS !== "web" && <BottomNav />}
+        </>
     )
 }
