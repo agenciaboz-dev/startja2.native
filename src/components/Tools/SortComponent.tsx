230027d9c8447f27253075ea1b3cef903acf8d71
alfredo-petri <alfredo_petri@hotmail.com>
2024-06-27 16:20:43 -0300

    Update SortComponent

--- src/components/Tools/SortComponent.tsx
+++ src/components/Tools/SortComponent.tsx
@@ -1,15 +1,17 @@
 import React, { useState } from "react"
 import { StyleProp, View, ViewStyle } from "react-native"
-import { Button, Icon, IconButton, Surface, Text, useTheme } from "react-native-paper"
+import { Button, Divider, Icon, IconButton, Menu, Surface, Text, useTheme } from "react-native-paper"
 import { colors } from "../../style/colors"
-import { SortModal } from "./SortModal"
+import { SortMenu } from "./SortMenu"
 
 interface SortComponentProps {
     title?: string
     style?: StyleProp<ViewStyle>
+    data: any[]
+    setSortedData: React.Dispatch<React.SetStateAction<any[]>>
 }
 
-export const SortComponent: React.FC<SortComponentProps> = ({ title, style }) => {
+export const SortComponent: React.FC<SortComponentProps> = ({ title, style, data, setSortedData }) => {
     const [openSortModal, setOpenSortModal] = useState<boolean>(false)
 
     const onDismiss = () => {
@@ -19,34 +21,51 @@ export const SortComponent: React.FC<SortComponentProps> = ({ title, style }) =>
     return (
         <>
             {title ? (
-                <Button
-                    icon="swap-vertical"
-                    onPress={() => setOpenSortModal(true)}
-                    style={[
-                        {
-                            backgroundColor: "white",
-                            borderColor: colors.grey,
-                            borderWidth: 1,
-                            borderRadius: 5,
-                            flexDirection: "row",
-                            alignItems: "center",
-                            justifyContent: "center",
-                            height: "auto",
-                        },
-                        style,
-                    ]}
+                <Menu
+                    visible={openSortModal}
+                    onDismiss={onDismiss}
+                    anchor={
+                        <Button
+                            icon="swap-vertical"
+                            onPress={() => setOpenSortModal(true)}
+                            style={[
+                                {
+                                    backgroundColor: "white",
+                                    borderColor: colors.grey,
+                                    borderWidth: 1,
+                                    borderRadius: 5,
+                                    flexDirection: "row",
+                                    alignItems: "center",
+                                    justifyContent: "center",
+                                    height: "auto",
+                                },
+                                style,
+                            ]}
+                        >
+                            {title}
+                        </Button>
+                    }
+                    contentStyle={{ padding: 0, backgroundColor: colors.box }}
                 >
-                    {title}
-                </Button>
+                    <SortMenu />
+                </Menu>
             ) : (
-                <IconButton
-                    icon="swap-vertical"
-                    iconColor={colors.primary}
-                    style={{ borderColor: colors.grey, borderWidth: 1, borderRadius: 5 }}
-                    onPress={() => setOpenSortModal(true)}
-                />
+                <Menu
+                    visible={openSortModal}
+                    onDismiss={onDismiss}
+                    anchor={
+                        <IconButton
+                            icon="swap-vertical"
+                            iconColor={colors.primary}
+                            style={{ borderColor: colors.grey, borderWidth: 1, borderRadius: 5 }}
+                            onPress={() => setOpenSortModal(true)}
+                        />
+                    }
+                >
+                    <SortMenu />
+                </Menu>
             )}
-            <SortModal visible={openSortModal} onDismiss={onDismiss} />
+            {/* <SortModal visible={openSortModal} onDismiss={onDismiss} /> */}
         </>
     )
 }
