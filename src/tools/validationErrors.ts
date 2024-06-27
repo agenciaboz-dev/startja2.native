b188e2ea097236c4d8406912af13203359afe25d
alfredo-petri <alfredo_petri@hotmail.com>
2024-06-27 06:26:19 -0300

    update validationErrors

--- src/tools/validationErrors.ts
+++ src/tools/validationErrors.ts
@@ -1,3 +1,42 @@
+import React, { useState } from "react"
+import { User } from "../types/server/class"
+
 export const validationErrors = {
     required: "Campo obrigatório.",
+    shortField: (fieldName: string) => `O campo ${fieldName} deve conter ao menos 3 caracteres.`,
+    mediumField: (fieldName: string) => `O campo ${fieldName} deve conter ao menos 5 caracteres.`,
+    invalidName: "Por favor, insira um nome de usuário válido. Evite usar caracteres especiais ou símbolos.",
+    invalidEmail: "Por favor, forneça um endereço de email válido.",
+    invalidPhone: "Por favor, forneça um número de telefone válido. Deve conter apenas números e ter o formato correto para seu país.",
+    invalidPasswordConfirmation: "Senha incorreta. Por favor, digite a senha atual para autorizar a mudança.",
+    invalidEmailConfirmation: "O endereço de email inserido não corresponde ao seu email atual.",
+    invalidPhoneConfirmation: "O número de telefone inserido não corresponde ao seu telefone atual.",
+}
+
+export const validatePassword = async (
+    currentPassword: string,
+    user: User,
+    setPasswordError: React.Dispatch<React.SetStateAction<boolean>>
+): Promise<boolean> => {
+    if (currentPassword !== user!.password) {
+        setPasswordError(true)
+        return false
+    } else {
+        setPasswordError(false)
+        return true
+    }
+}
+
+export const validateEmail = async (
+    currentEmail: string,
+    user: User,
+    setEmailError: React.Dispatch<React.SetStateAction<boolean>>
+): Promise<boolean> => {
+    if (currentEmail !== user!.email) {
+        setEmailError(true)
+        return false
+    } else {
+        setEmailError(false)
+        return true
+    }
 }
