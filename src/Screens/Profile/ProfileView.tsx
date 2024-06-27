0f75abeaaacce2d0205a0b2bdf85fa957327940c
ispadilha <israel.padilha@gmail.com>
2024-06-27 07:37:01 -0300

    style: Profile for mobile

--- src/Screens/Profile/ProfileView.tsx
+++ src/Screens/Profile/ProfileView.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react"
-import { View } from "react-native"
+import { Platform, View } from "react-native"
 import { Avatar, IconButton, Surface, Text, Button } from "react-native-paper"
 import AvatarImage from "react-native-paper/lib/typescript/components/Avatar/AvatarImage"
 import { useUser } from "../../hooks/useUser"
@@ -27,12 +27,21 @@ export const ProfileView: React.FC<ProfileViewProps> = ({}) => {
     const [openDeleteAccountModal, setOpenDeleteAccountModal] = useState(false)
 
     return (
-        <View style={{ flex: 1, paddingTop: 80, paddingLeft: 50, paddingRight: 500, gap: 40 }}>
+        <View
+            style={[
+                { flex: 1, paddingTop: 80, paddingLeft: 50, paddingRight: 500, gap: 40 },
+                Platform.OS !== "web" && { flex: 1, paddingTop: 20, paddingLeft: 20, paddingRight: 20, gap: 40 },
+            ]}
+        >
             <View style={{ flexDirection: "row", justifyContent: "space-between", alignItems: "center" }}>
                 <Text variant="headlineSmall">Minha Conta</Text>
-                <IconButton icon="close" />
+                {Platform.OS === "web" ? (
+                    <IconButton icon="close" />
+                ) : (
+                    <IconButton mode="contained" containerColor={colors.primary} iconColor="white" icon={"cog"} />
+                )}
             </View>
-            <Surface style={{ padding: 20, gap: 30, backgroundColor: colors.secondary }}>
+            <Surface style={[{ padding: 20, gap: 30, backgroundColor: colors.secondary }, Platform.OS !== "web" && { marginTop: -10 }]}>
                 <View style={{ flexDirection: "row", gap: 15, alignItems: "center" }}>
                     <Surface style={{ width: 61, height: 61, borderRadius: 50, position: "relative" }}>
                         <Avatar.Image
@@ -56,12 +65,23 @@ export const ProfileView: React.FC<ProfileViewProps> = ({}) => {
             </View>
             <View style={{ gap: 15 }}>
                 <Text variant="headlineSmall">Deletar conta</Text>
-                <Text> Desativar sua conta significa que você pode recuperá-la a qualquer momento após realizar esta ação</Text>
-                <View style={{ flexDirection: "row", gap: 15 }}>
-                    <Button mode="contained-tonal" textColor={colors.dark} onPress={() => setOpenDisableAccountModal(true)}>
+                <Text>Desativar sua conta significa que você pode recuperá-la a qualquer momento após realizar esta ação.</Text>
+                <View style={[{ flexDirection: "row", gap: 15 }, Platform.OS !== "web" && { flexDirection: "column" }]}>
+                    <Button
+                        mode="contained-tonal"
+                        textColor={colors.dark}
+                        onPress={() => setOpenDisableAccountModal(true)}
+                        style={[Platform.OS !== "web" && { alignSelf: "flex-start" }]}
+                    >
                         Desabilitar Conta
                     </Button>
-                    <Button mode="contained" buttonColor={colors.grey} dark onPress={() => setOpenDeleteAccountModal(true)}>
+                    <Button
+                        mode="contained"
+                        buttonColor={colors.grey}
+                        dark
+                        onPress={() => setOpenDeleteAccountModal(true)}
+                        style={[Platform.OS !== "web" && { alignSelf: "flex-start" }]}
+                    >
                         Deletar Conta
                     </Button>
                 </View>
