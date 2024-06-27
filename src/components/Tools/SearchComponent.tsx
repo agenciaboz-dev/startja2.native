8ff3acb39d8bf461bbe90ef1f316a70ec491d6f0
alfredo-petri <alfredo_petri@hotmail.com>
2024-06-27 13:19:23 -0300

    feat search on SearchComponent

--- src/components/Tools/SearchComponent.tsx
+++ src/components/Tools/SearchComponent.tsx
@@ -1,22 +1,32 @@
-import React from "react"
+import React, { useState } from "react"
 import { Surface, TextInput } from "react-native-paper"
+import { SystemComponentProp } from "../../types/SystemComponentProp"
 
 interface SearchComponentProps {
     placeholder?: string
+    data: any[]
+    setFilteredData: React.Dispatch<React.SetStateAction<any[]>>
 }
 
-export const SearchComponent: React.FC<SearchComponentProps> = ({ placeholder = "Procurar por nome, CPF, CNPJ, email..." }) => {
+export const SearchComponent: React.FC<SearchComponentProps> = ({
+    placeholder = "Procurar por nome, CPF, CNPJ, email...",
+    data,
+    setFilteredData,
+}) => {
+    const [search, setSearch] = useState("")
+    const handleSearch = (text: string) => {
+        setSearch(text)
+        setFilteredData(data.filter((data) => data.name.toLowerCase().includes(search.toLowerCase())))
+    }
+
     return (
         <TextInput
             mode="outlined"
             dense
             placeholder={placeholder}
-            // value={searchValue}
-            // onChangeText={(text) => setSearchValue(text)}
             right={<TextInput.Icon icon="magnify" />}
             style={[{ flex: 1 }]}
-            // onChange={(e) => console.log(e.target.value)}
-            onPress={() => console.log("clicando no searchField")}
+            onChangeText={(text) => handleSearch(text)}
         />
     )
 }
