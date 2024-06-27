59869b531e09f859aa3d9a66038dc0ef1336ea57
alfredo-petri <alfredo_petri@hotmail.com>
2024-06-27 09:24:39 -0300

    update UserPhoneModal

--- src/Screens/Profile/Modals/UserPhoneModal.tsx
+++ src/Screens/Profile/Modals/UserPhoneModal.tsx
@@ -1,7 +1,14 @@
-import React, { useState } from "react"
+import React, { useContext, useState } from "react"
 import { Platform, View } from "react-native"
 import { Button, IconButton, Modal, Portal, Text, TextInput } from "react-native-paper"
 import { colors } from "../../../style/colors"
+import { useUser } from "../../../hooks/useUser"
+import UserContext from "../../../contexts/userContext"
+import * as Yup from "yup"
+import { validatePassword, validatePhone, validationErrors } from "../../../tools/validationErrors"
+import { useFormik } from "formik"
+import { PartialUser } from "../../../types/server/class/User"
+import { api } from "../../../backend/api"
 
 interface UserPhoneModalProps {
     visible: boolean
@@ -9,9 +16,38 @@ interface UserPhoneModalProps {
 }
 
 export const UserPhoneModal: React.FC<UserPhoneModalProps> = ({ visible, onDismiss }) => {
-    const [phoneActive, setPhoneActive] = useState(false)
+    const { user } = useUser()
+    const context = useContext(UserContext)
+
+    const [currentPhoneActive, setCurrentPhoneActive] = useState(false)
     const [newPhoneActive, setNewPhoneActive] = useState(false)
     const [passwordActive, setPasswordActive] = useState(false)
+
+    const [currentPassword, setCurrentPassword] = useState<string>("")
+    const [currentPasswordError, setCurrentPasswordError] = useState(false)
+    const [currentPhone, setCurrentPhone] = useState<string>("")
+    const [currentPhoneError, setCurrentPhoneError] = useState(false)
+
+    //todo: melhorar a validação do phone
+    const validateSchema = Yup.object().shape({
+        phone: Yup.string().min(11, validationErrors.invalidPhone).required(validationErrors.required),
+    })
+
+    const formik = useFormik<PartialUser>({
+        initialValues: { id: user!.id, phone: "" },
+        async onSubmit(values) {
+            try {
+                const response = await api.patch("/user", values)
+                context.setUser(response.data)
+                formik.resetForm()
+            } catch (error) {
+                console.log(error)
+            }
+        },
+        validationSchema: validateSchema,
+        validateOnChange: false,
+        enableReinitialize: true,
+    })
 
     return (
         <Portal>
@@ -36,28 +72,70 @@ export const UserPhoneModal: React.FC<UserPhoneModalProps> = ({ visible, onDismi
                         <IconButton icon="close" onPress={onDismiss} />
                     </View>
                     <Text>Atualize seu número de telefone para garantir que podemos contatá-lo quando necessário.</Text>
+
                     <TextInput
+                        value={currentPhone}
                         mode="outlined"
                         placeholder="Digite seu telefone atual"
-                        label={phoneActive ? "Telefone atual" : "Digite seu telefone atual"}
-                        onFocus={() => setPhoneActive(true)}
-                        onBlur={() => setPhoneActive(false)}
+                        label={currentPhoneActive ? "Telefone atual" : "Digite seu telefone atual"}
+                        onFocus={() => setCurrentPhoneActive(true)}
+                        onBlur={() => setCurrentPhoneActive(false)}
+                        onChangeText={(phone) => {
+                            setCurrentPhone(phone)
+                        }}
                     />
+                    {currentPhoneError && <Text style={{ color: colors.error, marginTop: -20 }}>{validationErrors.invalidPhoneConfirmation}</Text>}
+
                     <TextInput
+                        value={formik.values.phone}
                         mode="outlined"
                         placeholder="Digite seu novo telefone"
                         label={newPhoneActive ? "Novo telefone" : "Digite seu novo telefone"}
                         onFocus={() => setNewPhoneActive(true)}
-                        onBlur={() => setNewPhoneActive(false)}
+                        onBlur={() => {
+                            formik.handleBlur("phone")
+                            setNewPhoneActive(false)
+                        }}
+                        onChangeText={formik.handleChange("phone")}
                     />
+                    {formik.touched.phone && formik.errors.phone && (
+                        <Text style={{ color: colors.error, marginTop: -20 }}>{formik.errors.phone}</Text>
+                    )}
+
                     <TextInput
+                        value={currentPassword}
                         mode="outlined"
                         placeholder="Insira sua senha"
                         label={passwordActive ? "Senha" : "Insira sua senha"}
                         onFocus={() => setPasswordActive(true)}
-                        onBlur={() => setPasswordActive(false)}
+                        onBlur={() => {
+                            setPasswordActive(false)
+                        }}
+                        onChangeText={(password) => {
+                            setCurrentPassword(password)
+                        }}
+                        secureTextEntry={true}
                     />
-                    <Button mode="contained" style={{ alignSelf: "flex-end" }}>
+                    {currentPasswordError && (
+                        <Text style={{ color: colors.error, marginTop: -20 }}>{validationErrors.invalidPasswordConfirmation}</Text>
+                    )}
+
+                    <Button
+                        mode="contained"
+                        style={{ alignSelf: "flex-end" }}
+                        onPress={() => {
+                            validatePhone(currentPhone, user!, setCurrentPhoneError)
+                            validatePassword(currentPassword, user!, setCurrentPasswordError).then((isValid) => {
+                                console.log(isValid)
+                                if (isValid) {
+                                    formik.handleSubmit()
+                                    onDismiss()
+                                    setCurrentPassword("")
+                                    setCurrentPhone("")
+                                }
+                            })
+                        }}
+                    >
                         Atualizar
                     </Button>
                 </View>
