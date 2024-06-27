658e40e34c4c2c964206d890c3305551fd40b705
alfredo-petri <alfredo_petri@hotmail.com>
2024-06-27 15:16:01 -0300

    update SortComponent

--- src/components/Tools/SortComponent.tsx
+++ src/components/Tools/SortComponent.tsx
@@ -1,7 +1,8 @@
-import React from "react"
+import React, { useState } from "react"
 import { StyleProp, View, ViewStyle } from "react-native"
 import { Button, Icon, IconButton, Surface, Text, useTheme } from "react-native-paper"
 import { colors } from "../../style/colors"
+import { SortModal } from "./SortModal"
 
 interface SortComponentProps {
     title?: string
@@ -9,34 +10,43 @@ interface SortComponentProps {
 }
 
 export const SortComponent: React.FC<SortComponentProps> = ({ title, style }) => {
-    return title ? (
-        <Button
-            icon="swap-vertical"
-            onPress={() => console.log("clicou no sort")}
-            style={[
-                {
-                    backgroundColor: "white",
-                    borderColor: colors.grey,
-                    borderWidth: 1,
-                    borderRadius: 5,
-                    flexDirection: "row",
-                    alignItems: "center",
-                    justifyContent: "center",
-                    height: "auto",
-                },
-                style,
-            ]}
-        >
-            {title}
-        </Button>
-    ) : (
-        <IconButton
-            icon="swap-vertical"
-            iconColor={colors.primary}
-            style={{ borderColor: colors.grey, borderWidth: 1, borderRadius: 5 }}
-            onPress={() => console.log("clicou no sort")}
-        />
+    const [openSortModal, setOpenSortModal] = useState<boolean>(false)
+
+    const onDismiss = () => {
+        setOpenSortModal(false)
+    }
+
+    return (
+        <>
+            {title ? (
+                <Button
+                    icon="swap-vertical"
+                    onPress={() => setOpenSortModal(true)}
+                    style={[
+                        {
+                            backgroundColor: "white",
+                            borderColor: colors.grey,
+                            borderWidth: 1,
+                            borderRadius: 5,
+                            flexDirection: "row",
+                            alignItems: "center",
+                            justifyContent: "center",
+                            height: "auto",
+                        },
+                        style,
+                    ]}
+                >
+                    {title}
+                </Button>
+            ) : (
+                <IconButton
+                    icon="swap-vertical"
+                    iconColor={colors.primary}
+                    style={{ borderColor: colors.grey, borderWidth: 1, borderRadius: 5 }}
+                    onPress={() => setOpenSortModal(true)}
+                />
+            )}
+            <SortModal visible={openSortModal} onDismiss={onDismiss} />
+        </>
     )
 }
-
-
