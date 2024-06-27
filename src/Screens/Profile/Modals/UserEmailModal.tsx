7d3075886f379059feda9f5b162c3731b5e787ae
alfredo-petri <alfredo_petri@hotmail.com>
2024-06-27 08:45:04 -0300

    update UserEmailModal

--- src/Screens/Profile/Modals/UserEmailModal.tsx
+++ src/Screens/Profile/Modals/UserEmailModal.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react"
+import React, { useContext, useState } from "react"
 import { Platform, View } from "react-native"
 import { Button, IconButton, Modal, Portal, Text, TextInput } from "react-native-paper"
 import { colors } from "../../../style/colors"
@@ -7,6 +7,8 @@ import { validateEmail, validatePassword, validationErrors } from "../../../tool
 import { useFormik } from "formik"
 import { PartialUser } from "../../../types/server/class/User"
 import { useUser } from "../../../hooks/useUser"
+import UserContext from "../../../contexts/userContext"
+import { api } from "../../../backend/api"
 
 interface UserEmailModalProps {
     visible: boolean
@@ -14,23 +16,34 @@ interface UserEmailModalProps {
 }
 
 export const UserEmailModal: React.FC<UserEmailModalProps> = ({ visible, onDismiss }) => {
+    const emailRegex = (value: string) => /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(value)
     const { user } = useUser()
-    const [emailActive, setEmailActive] = useState(false)
+    const context = useContext(UserContext)
+
+    const [currentEmailActive, setCurrentEmailActive] = useState(false)
     const [newEmailActive, setNewEmailActive] = useState(false)
     const [passwordActive, setPasswordActive] = useState(false)
     const [currentPassword, setCurrentPassword] = useState<string>("")
     const [passwordError, setPasswordError] = useState(false)
     const [currentEmail, setCurrentEmail] = useState<string>("")
-    const [emailError, setEmailError] = useState(false)
+    const [currentEmailError, setCurrentEmailError] = useState(false)
+
+    //todo: melhorar a validação a mesma está incorreta
 
     const validateSchema = Yup.object().shape({
-        email: Yup.string().email(validationErrors.invalidEmail).required(validationErrors.required),
+        email: Yup.string().required(validationErrors.required).test("emailregex", validationErrors.invalidEmail, emailRegex),
     })
 
     const formik = useFormik<PartialUser>({
         initialValues: { id: user!.id, email: "" },
-        onSubmit(values) {
-            console.log(`enviando: ${values}`)
+        async onSubmit(values) {
+            try {
+                const response = await api.patch("/user", values)
+                context.setUser(response.data)
+                formik.resetForm()
+            } catch (error) {
+                console.log(error)
+            }
         },
         validationSchema: validateSchema,
         validateOnChange: false,
@@ -59,36 +72,41 @@ export const UserEmailModal: React.FC<UserEmailModalProps> = ({ visible, onDismi
                         <IconButton icon="close" onPress={onDismiss} />
                     </View>
                     <Text>Forneça um endereço de email válido para receber notificações e recuperar sua conta</Text>
+
                     <TextInput
+                        value={currentEmail}
                         mode="outlined"
                         placeholder="Digite o endereço de email atual"
-                        label={emailActive ? "Endereço de email atual" : "Digite o endereço de email atual"}
-                        onFocus={() => setEmailActive(true)}
-                        onBlur={() => setEmailActive(false)}
+                        label={currentEmailActive ? "Endereço de email atual" : "Digite o endereço de email atual"}
+                        onFocus={() => setCurrentEmailActive(true)}
+                        onBlur={() => {
+                            setCurrentEmailActive(false)
+                        }}
                         onChangeText={(email) => {
                             setCurrentEmail(email)
                         }}
                     />
-                    {emailError && <Text style={{ color: colors.error, marginTop: -20 }}>{validationErrors.invalidEmailConfirmation}</Text>}
+                    {currentEmailError && <Text style={{ color: colors.error, marginTop: -20 }}>{validationErrors.invalidEmailConfirmation}</Text>}
 
                     <TextInput
+                        value={formik.values.email}
                         mode="outlined"
                         placeholder="Digite o novo endereço de email"
                         label={newEmailActive ? "Novo email" : "Digite o novo endereço de email"}
                         onFocus={() => setNewEmailActive(true)}
                         onBlur={() => {
-                            setNewEmailActive(false)
                             formik.handleBlur("email")
+                            setNewEmailActive(false)
                         }}
-                        onChangeText={formik.handleChange}
+                        onChangeText={formik.handleChange("email")}
                     />
                     {formik.touched.email && formik.errors.email && (
                         <Text style={{ color: colors.error, marginTop: -20 }}>{formik.errors.email}</Text>
                     )}
 
                     <TextInput
-                        mode="outlined"
                         value={currentPassword}
+                        mode="outlined"
                         placeholder="Insira sua senha"
                         label={passwordActive ? "Senha" : "Insira sua senha"}
                         onFocus={() => setPasswordActive(true)}
@@ -106,13 +124,14 @@ export const UserEmailModal: React.FC<UserEmailModalProps> = ({ visible, onDismi
                         mode="contained"
                         style={{ alignSelf: "flex-end" }}
                         onPress={() => {
-                            validateEmail(currentEmail, user!, setEmailError).then((isvalid) => {
-                                // console.log({ emaiValido: isvalid })
-                            })
+                            validateEmail(currentEmail, user!, setCurrentEmailError)
                             validatePassword(currentPassword, user!, setPasswordError).then((isValid) => {
-                                // console.log(isValid)
+                                console.log(isValid)
                                 if (isValid) {
                                     formik.handleSubmit()
+                                    onDismiss()
+                                    setCurrentPassword("")
+                                    setCurrentEmail("")
                                 }
                             })
                         }}
